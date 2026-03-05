deef5ceee9bfc8c3a70a9afdb6e5b182ce8802aa
nickleusjimenez <nickleus.jimenez@gmail.com>
2026-03-05 11:35:17 +0800

    Added Swagger

--- SvgMaker/Program.cs
+++ SvgMaker/Program.cs
@@ -2,7 +2,9 @@ using SvgMaker.Lib;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -10,22 +12,30 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();   // Serves the generated JSON document
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    }); // Serves the interactive UI
+}
 app.UseHttpsRedirection();
 var svgGen = SvgGenerator.Instance;
 app.MapGet("/", () => "Hello There! try /random");
 app.MapGet("/random", () => 
     Results.Content(svgGen.GenerateRandomSvg(330,250)
         ,"image/svg+xml; charset=utf-8"));
-app.MapGet("/random/{width}/{height}", (int width, int height) => 
+app.MapGet("/random/{width:int}/{height:int}", (int width, int height) => 
     Results.Content(svgGen.GenerateRandomSvg(width,height),"image/svg+xml; charset=utf-8"));
-app.MapGet("/rectangle/{width}/{height}", (int width, int height) => 
+app.MapGet("/rectangle/{width:int}/{height:int}", (int width, int height) => 
     Results.Content(svgGen.GenerateRandomRectanglesSvg(10,width,height),"image/svg+xml; charset=utf-8"));
-app.MapGet("/circles/{width}/{height}", (int width, int height) => 
+app.MapGet("/circles/{width:int}/{height:int}", (int width, int height) => 
     Results.Content(svgGen.GenerateCirclesSvg(10,width,height),"image/svg+xml; charset=utf-8"));
-app.MapGet("/circle/{width}/{height}", (int width, int height) => 
+app.MapGet("/circle/{width:int}/{height:int}", (int width, int height) => 
     Results.Content(svgGen.GenerateCircleSvg(width,height),"image/svg+xml; charset=utf-8"));
-app.MapGet("/circle/{radius}", (int radius) => 
+app.MapGet("/circle/{radius:int}", (int radius) => 
     Results.Content(svgGen.GenerateCircleSvg(radius),"image/svg+xml; charset=utf-8"));
 app.MapGet("/write/{text}", (string text) => 
     Results.Content(svgGen.GenerateSvgText(text),"image/svg+xml; charset=utf-8"));
